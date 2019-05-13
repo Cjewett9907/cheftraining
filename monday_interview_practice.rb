98a96f919cd2e6455847cd3fd93fa2cbb6cc69f2
Cjewett_work <Craig9907@gmail.com>
2019-05-13 14:18:29 -0700

    intial push of devop studying shcedule

---
+++ monday_interview_practice.rb
@@ -0,0 +1,14 @@
+def learn_chef_commands do
+    # write sample chef code
+    # study 'supermarket' examples
+    # memorize key commands
+end
+
+def learn_linux_commands do
+    # memorize keywords (ls, ps, pwd, top, man, netstat)
+end
+
+def learn_devops_trivia do
+    # great to have 
+    # not_if time_remaining < 1 day
+end
